d455bb47ba36e8eba6057628e5ac43dc17d60176
R3v3rS <37628848+R3v3rS@users.noreply.github.com>
2026-04-13 22:43:37 +0200

    feat: import FIT + metryki NP/IF/TSS

---
+++ apps/api/src/domain/metrics/__init__.py
@@ -0,0 +1,15 @@
+from .training_load import (
+    MetricsError,
+    calculate_if,
+    calculate_metrics,
+    calculate_np,
+    calculate_tss,
+)
+
+__all__ = [
+    "MetricsError",
+    "calculate_np",
+    "calculate_if",
+    "calculate_tss",
+    "calculate_metrics",
+]
